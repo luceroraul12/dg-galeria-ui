e84e9df6033aae7a4cb73e41b8cb842cf95a0ba6
Lucero Raul <luceroraul12@gmail.com>
2022-09-05 12:16:41 -0300

    add vista detallada component

--- src/app/paginas/cliente/cliente.module.ts
+++ src/app/paginas/cliente/cliente.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VistaClienteComponent } from './vista-cliente/vista-cliente.component';
 import { GaleriaComponent } from './vista-cliente/components/galeria/galeria.component';
+import { VistaDetalladaComponent } from './vista-cliente/components/vista-detallada/vista-detallada.component';
 
 
 
 @NgModule({
   declarations: [
     VistaClienteComponent,
-    GaleriaComponent
+    GaleriaComponent,
+    VistaDetalladaComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/paginas/cliente/vista-cliente/components/vista-detallada/vista-detallada.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-detallada',
+  templateUrl: './vista-detallada.component.html',
+  styleUrls: ['./vista-detallada.component.css']
+})
+export class VistaDetalladaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
