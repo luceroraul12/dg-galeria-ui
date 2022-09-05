7c2cb540bd2d3ba0fc240d723aa75ac636a26d7b
Lucero Raul <luceroraul12@gmail.com>
2022-09-05 09:02:13 -0300

    add modulos y componente barra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdministradorModule } from './paginas/administrador/administrador.module';
+import { ClienteModule } from './paginas/cliente/cliente.module';
+import { MenuModule } from './menu/menu.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenuModule,
+    AdministradorModule,
+    ClienteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
