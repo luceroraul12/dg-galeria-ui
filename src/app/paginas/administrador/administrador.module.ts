b2c51d5db490f9b7434bf92c5d645f8a6f6aee05
Lucero Raul <luceroraul12@gmail.com>
2022-09-05 09:34:47 -0300

    add export

--- src/app/paginas/administrador/administrador.module.ts
+++ src/app/paginas/administrador/administrador.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VistaAdministradorComponent } from './vista-administrador/vista-administrador.component';
+import { BebidaFormularioComponent } from './vista-administrador/components/bebida-formulario/bebida-formulario.component';
 
 
 
 @NgModule({
   declarations: [
-    VistaAdministradorComponent
+    VistaAdministradorComponent,
+    BebidaFormularioComponent
   ],
   imports: [
     CommonModule
