4c6c12f114a25f37c6e3d1bd0f1730f4c279a6c0
Lucero Raul <luceroraul12@gmail.com>
2022-09-05 11:07:47 -0300

    add mas ejemplos

--- src/app/servicios/cliente.service.ts
+++ src/app/servicios/cliente.service.ts
@@ -18,6 +18,18 @@ export class ClienteService {
     {
       nombre: "Heineken",
       enlaceImagen: "https://scontent.fluq3-1.fna.fbcdn.net/v/t1.6435-9/37568671_1859067904113633_1881463312240082944_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHhpIpkUM-tMTXjisSGIIXQOvgd6CdTH5A6-B3oJ1MfkHCVG4agwpTrUxK3X6bLIUrFlY2QpKSrgXC1dPAPbaLJ&_nc_ohc=hUDK8rBPmh4AX_xTVeH&_nc_ht=scontent.fluq3-1.fna&oh=00_AT9SjbSQBMaGusXs8Z4n1fDi3O8LurPy8KQA-uKaH_YdEA&oe=633B0881"
+    },
+    {
+      nombre: "Isenbeck",
+      enlaceImagen: "https://scontent.fluq3-1.fna.fbcdn.net/v/t1.18169-9/17103302_10154408796478061_3474089334054338498_n.png?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFK4PXb6aPiim-YsdJyrArtm96keNnWTL-b3qR42dZMv_tRDCvsiGDRA1OuhP9qjLFpu0ci4dtllx5wN99el0qx&_nc_ohc=Ytkh-1OPzf0AX-VzVqp&_nc_ht=scontent.fluq3-1.fna&oh=00_AT8z2CJPis_252t0FeQPA6URLBEkFiWuK_Y6Mg-ju9ZJhw&oe=6339C3E9"
+    },
+    {
+      nombre: "Valmont",
+      enlaceImagen: "https://scontent.fluq3-1.fna.fbcdn.net/v/t1.18169-9/22046605_1675749652444909_4086562462651098789_n.png?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG-C78a8xQfNERWpOzm8HK-M1S007NoskYzVLTTs2iyRh4EeM6T-JtVi3PMx8V4s7b0Nzxl6J_0D_g1BC2BhQaX&_nc_ohc=26flt5B1qVkAX_N6AUg&_nc_ht=scontent.fluq3-1.fna&oh=00_AT_wmqkU-EpR5U-xQ3eyfxqve5uAEyoM3uHU9xMyaC2SUQ&oe=633D572F"
+    },
+    {
+      nombre: "Estancia Mendoza",
+      enlaceImagen: "https://scontent.fluq3-1.fna.fbcdn.net/v/t1.6435-9/64607539_2360357210674284_3657424369216389120_n.png?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEZ0mZTL8K0Do7esZzGQTDo9pllN0rqV4z2mWU3SupXjKksb42UMxmK2YjaTy7S2-s6xvdcqFzzKNHYm1BnH4US&_nc_ohc=RkMMNaIpy9sAX8jn9lN&_nc_ht=scontent.fluq3-1.fna&oh=00_AT9IiSMn68H4tEaopDwmQXV2lhZfeP1zhoM2w98kaE_SsA&oe=633A6DC6"
     }
   ]
 
@@ -28,5 +40,5 @@ export class ClienteService {
 
   constructor() { }
 
-  
+
 }
