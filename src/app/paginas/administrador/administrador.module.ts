1bdbda07b79a90213b2d32c2109bb8a736b7f335
Lucero Raul <luceroraul12@gmail.com>
2022-09-05 10:12:36 -0300

    add formato formulario

--- src/app/paginas/administrador/administrador.module.ts
+++ src/app/paginas/administrador/administrador.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { VistaAdministradorComponent } from './vista-administrador/vista-administrador.component';
 import { BebidaFormularioComponent } from './vista-administrador/components/bebida-formulario/bebida-formulario.component';
 import { MarcaFormularioComponent } from './vista-administrador/components/marca-formulario/marca-formulario.component';
+import { FormatoFormularioComponent } from './vista-administrador/components/formato-formulario/formato-formulario.component';
 
 
 
@@ -10,7 +11,8 @@ import { MarcaFormularioComponent } from './vista-administrador/components/marca
   declarations: [
     VistaAdministradorComponent,
     BebidaFormularioComponent,
-    MarcaFormularioComponent
+    MarcaFormularioComponent,
+    FormatoFormularioComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/paginas/administrador/vista-administrador/components/formato-formulario/formato-formulario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formato-formulario',
+  templateUrl: './formato-formulario.component.html',
+  styleUrls: ['./formato-formulario.component.css']
+})
+export class FormatoFormularioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
