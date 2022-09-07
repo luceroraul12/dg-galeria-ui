ef078cd49c1bf488a513c235ab0d734ae33f2a26
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 19:48:45 -0300

    add observables

--- src/app/servicios/cliente.service.ts
+++ src/app/servicios/cliente.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Formato, GaleriaResponse } from '../interfaces/galeria.interface';
+import {
+  Formato,
+  GaleriaResponse,
+  Marca,
+} from '../interfaces/galeria.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +15,23 @@ export class ClienteService {
 
   constructor(private http: HttpClient) {}
 
-  formatosCreados(): Observable<Formato> {
+  formatosCreados(): Observable<GaleriaResponse> {
     let url: string = `${this.urlApi}/formatos`;
-    return this.http.get<Formato>(url);
+    return this.http.get<GaleriaResponse>(url);
+  }
+
+  marcasCreadas(): Observable<GaleriaResponse> {
+    let url: string = `${this.urlApi}/marcas`;
+    return this.http.get<GaleriaResponse>(url);
+  }
+
+  saboresCreadosByIdMarca(idMarca: string): Observable<GaleriaResponse> {
+    let url: string = `${this.urlApi}/sabores`;
+    return this.http.get<GaleriaResponse>(url);
+  }
+
+  saboresCreadosByIdMarcayStock(idMarca: string): Observable<GaleriaResponse> {
+    let url: string = `${this.urlApi}/sabores/con-stock`;
+    return this.http.get<GaleriaResponse>(url);
   }
 }
