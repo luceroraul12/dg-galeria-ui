6b1645aafb5efaaedf10a011afcfa35c0f19a9a4
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 19:52:07 -0300

    add ruta dinamica por id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VistaAdministradorComponent } from './paginas/administrador/vista-administrador/vista-administrador.component';
 import { VistaClienteComponent } from './paginas/cliente/vista-cliente/vista-cliente.component';
+import { VistaDetalladaComponent } from './paginas/cliente/vista-cliente/components/vista-detallada/vista-detallada.component';
 
 const routes: Routes = [
   {
-    path: "cliente",
-    component: VistaClienteComponent
+    path: 'cliente',
+    component: VistaClienteComponent,
   },
   {
-    path: "administrador",
-    component: VistaAdministradorComponent
+    path: 'cliente/:idMarca',
+    component: VistaDetalladaComponent,
   },
   {
-    path: "**",
-    redirectTo: "cliente"
-  }
+    path: 'administrador',
+    component: VistaAdministradorComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'cliente',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
