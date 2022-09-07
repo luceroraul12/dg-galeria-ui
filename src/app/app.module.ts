1bf01e4d3bdaf9fc6338e4777a28b21117aa7141
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 19:16:33 -0300

    add http cliente module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,17 +9,16 @@ import { ClienteModule } from './paginas/cliente/cliente.module';
 import { MenuModule } from './menu/menu.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     MenuModule,
     AdministradorModule,
-    ClienteModule
+    ClienteModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
