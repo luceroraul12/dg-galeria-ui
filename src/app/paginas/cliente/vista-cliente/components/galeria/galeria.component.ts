8063a3e4e09335721db86b3125e94c3dc3a45031
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 19:27:50 -0300

    fix informacion de momento

--- src/app/paginas/cliente/vista-cliente/components/galeria/galeria.component.ts
+++ src/app/paginas/cliente/vista-cliente/components/galeria/galeria.component.ts
@@ -1,34 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ClienteService } from '../../../../../servicios/cliente.service';
-import { Sabor } from '../../../../../interfaces/sabor.interface';
+import { Sabor } from '../../../../../interfaces/galeria.interface';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
-  styleUrls: ['./galeria.component.css']
+  styleUrls: ['./galeria.component.css'],
 })
 export class GaleriaComponent implements OnInit {
-
   public fueClickeado: boolean;
 
-  get informacionDisponible(){
-    return this.clienteService.saboresDisponibles;
-  }
+  // get informacionDisponible(){
+  //   return this.clienteService.saboresDisponibles;
+  // }
 
-  constructor(private clienteService: ClienteService) { 
+  public informacionDisponible: Sabor[] = [];
+
+  constructor(private clienteService: ClienteService) {
     this.fueClickeado = false;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  clickearMarcar(SaborElegido: Sabor){
+  clickearMarcar(SaborElegido: Sabor) {
     console.log(SaborElegido);
     this.fueClickeado = true;
   }
 
-  escucharRespuesta(){
+  escucharRespuesta() {
     this.fueClickeado = false;
   }
-
 }
