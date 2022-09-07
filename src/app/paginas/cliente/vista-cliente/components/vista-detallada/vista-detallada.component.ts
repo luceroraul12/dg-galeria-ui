eba5d24f1dad619b4295a8e4ef87b83434b093e5
Lucero Raul <luceroraul12@gmail.com>
2022-09-07 20:41:21 -0300

    add observables

--- src/app/paginas/cliente/vista-cliente/components/vista-detallada/vista-detallada.component.ts
+++ src/app/paginas/cliente/vista-cliente/components/vista-detallada/vista-detallada.component.ts
@@ -1,23 +1,33 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ClienteService } from '../../../../../servicios/cliente.service';
 
 @Component({
   selector: 'app-vista-detallada',
   templateUrl: './vista-detallada.component.html',
-  styleUrls: ['./vista-detallada.component.css']
+  styleUrls: ['./vista-detallada.component.css'],
 })
 export class VistaDetalladaComponent implements OnInit {
-
   @Input() fueClickeado!: boolean;
   @Output() fueClickeadoCambio = new EventEmitter<boolean>();
 
+  private idMarca: string = '';
 
-  constructor() { }
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private clienteService: ClienteService
+  ) {}
 
   ngOnInit(): void {
+    this.activateRoute.params.subscribe(
+      ({ idMarca }) => (this.idMarca = idMarca)
+    );
+    this.clienteService
+      .saboresCreadosByIdMarcayStock(this.idMarca)
+      .subscribe(console.log);
   }
 
-  volver(){
+  volver() {
     this.fueClickeadoCambio.emit(this.fueClickeado);
   }
-
 }
