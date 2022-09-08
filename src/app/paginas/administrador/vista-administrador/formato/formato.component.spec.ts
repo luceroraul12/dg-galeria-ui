d895530fe82a21d22c39bc153dfdb72775aec627
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 09:17:05 -0300

    gen formato c

---
+++ src/app/paginas/administrador/vista-administrador/formato/formato.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormatoComponent } from './formato.component';
+
+describe('FormatoComponent', () => {
+  let component: FormatoComponent;
+  let fixture: ComponentFixture<FormatoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FormatoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FormatoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/paginas/administrador/vista-administrador/formato/formato.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formato',
+  templateUrl: './formato.component.html',
+  styleUrls: ['./formato.component.css']
+})
+export class FormatoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
