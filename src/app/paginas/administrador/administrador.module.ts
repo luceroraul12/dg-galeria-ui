9f74ef027437b78227d4d4f863e8884b2cac1a23
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 09:38:18 -0300

    cambiando de lugar componentes

--- src/app/paginas/administrador/administrador.module.ts
+++ src/app/paginas/administrador/administrador.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VistaAdministradorComponent } from './vista-administrador/vista-administrador.component';
-import { BebidaFormularioComponent } from './vista-administrador/components/bebida-formulario/bebida-formulario.component';
-import { MarcaFormularioComponent } from './vista-administrador/components/marca-formulario/marca-formulario.component';
-import { FormatoFormularioComponent } from './vista-administrador/components/formato-formulario/formato-formulario.component';
+import { BebidaFormularioComponent } from './vista-administrador/sabor/components/bebida-formulario/bebida-formulario.component';
+import { MarcaFormularioComponent } from './vista-administrador/marca/components/marca-formulario/marca-formulario.component';
+import { FormatoFormularioComponent } from './vista-administrador/formato/components/formato-formulario/formato-formulario.component';
 import { AppRoutingModule } from '../../app-routing.module';
 import { FormatoComponent } from './vista-administrador/formato/formato.component';
 import { MarcaComponent } from './vista-administrador/marca/marca.component';
--- src/app/paginas/administrador/vista-administrador/sabor/sabor.component.ts
+++ src/app/paginas/administrador/vista-administrador/sabor/sabor.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sabor',
   templateUrl: './sabor.component.html',
-  styleUrls: ['./sabor.component.css']
+  styleUrls: ['./sabor.component.css'],
 })
 export class SaborComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
