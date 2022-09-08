1dfd4bca91efbfe7314ed561120f8849a91e4d2e
Lucero Raul <luceroraul12@gmail.com>
2022-09-08 09:17:14 -0300

    declaracion de c

--- src/app/paginas/administrador/administrador.module.ts
+++ src/app/paginas/administrador/administrador.module.ts
@@ -4,21 +4,22 @@ import { VistaAdministradorComponent } from './vista-administrador/vista-adminis
 import { BebidaFormularioComponent } from './vista-administrador/components/bebida-formulario/bebida-formulario.component';
 import { MarcaFormularioComponent } from './vista-administrador/components/marca-formulario/marca-formulario.component';
 import { FormatoFormularioComponent } from './vista-administrador/components/formato-formulario/formato-formulario.component';
-
-
+import { AppRoutingModule } from '../../app-routing.module';
+import { FormatoComponent } from './vista-administrador/formato/formato.component';
+import { MarcaComponent } from './vista-administrador/marca/marca.component';
+import { SaborComponent } from './vista-administrador/sabor/sabor.component';
 
 @NgModule({
   declarations: [
     VistaAdministradorComponent,
     BebidaFormularioComponent,
     MarcaFormularioComponent,
-    FormatoFormularioComponent
+    FormatoFormularioComponent,
+    FormatoComponent,
+    MarcaComponent,
+    SaborComponent,
   ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    VistaAdministradorComponent
-  ]
+  imports: [CommonModule, AppRoutingModule],
+  exports: [VistaAdministradorComponent],
 })
-export class AdministradorModule { }
+export class AdministradorModule {}
