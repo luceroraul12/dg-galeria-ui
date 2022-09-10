cf6d314d139c3a8852a07721daa4ef0900cd39a8
Lucero Raul <luceroraul12@gmail.com>
2022-09-09 22:31:25 -0300

    delete todo lo que tiene que ver con galeriaresponse

--- src/app/paginas/cliente/vista-cliente/components/vista-detallada/vista-detallada.component.ts
+++ src/app/paginas/cliente/vista-cliente/components/vista-detallada/vista-detallada.component.ts
@@ -1,11 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ClienteService } from '../../../../../servicios/cliente.service';
-import {
-  Sabor,
-  Marca,
-  SaborResumido,
-} from '../../../../../interfaces/galeria.interface';
 
 @Component({
   selector: 'app-vista-detallada',
@@ -13,30 +7,11 @@ import {
   styleUrls: ['./vista-detallada.component.css'],
 })
 export class VistaDetalladaComponent implements OnInit {
-  public marcaElegida!: Marca;
   @Output() fueClickeadoCambio = new EventEmitter<boolean>();
-
-  public saboresDisponibles: SaborResumido[] = [];
 
   private idMarca: string = '';
 
-  constructor(
-    private activateRoute: ActivatedRoute,
-    private clienteService: ClienteService
-  ) {}
+  constructor(private activateRoute: ActivatedRoute) {}
 
-  ngOnInit(): void {
-    this.activateRoute.params.subscribe(({ idMarca }) => {
-      this.clienteService
-        .marcaById(idMarca)
-        .subscribe(
-          (respuesta) => (this.marcaElegida = respuesta.marcaTrabajada)
-        );
-      this.clienteService
-        .saboresCreadosByIdMarcaParaVer(idMarca)
-        .subscribe(
-          (respuesta) => (this.saboresDisponibles = respuesta.saboresResumidos)
-        );
-    });
-  }
+  ngOnInit(): void {}
 }
