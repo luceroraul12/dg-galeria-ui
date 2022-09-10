cfec69adc277713cf04d515cb6f7ae9be3525d8b
Lucero Raul <luceroraul12@gmail.com>
2022-09-10 15:46:39 -0300

    add tablaforma

--- src/app/paginas/administrador/vista-administrador/marca/marca-estado/components/tabla-marca/tabla-marca.component.ts
+++ src/app/paginas/administrador/vista-administrador/marca/marca-estado/components/tabla-marca/tabla-marca.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Marca } from 'src/app/interfaces/marca.interface';
+import { MarcaService } from 'src/app/servicios/marca.service';
 
 @Component({
   selector: 'app-tabla-marca',
   templateUrl: './tabla-marca.component.html',
-  styleUrls: ['./tabla-marca.component.css']
+  styleUrls: ['./tabla-marca.component.css'],
 })
 export class TablaMarcaComponent implements OnInit {
+  public marcasCreadas: Marca[] = [];
 
-  constructor() { }
+  constructor(private marcaService: MarcaService) {}
 
   ngOnInit(): void {
+    this.marcaService.getAllMarcas().subscribe((respuesta) => {
+      this.marcasCreadas = respuesta.marcasTrabajadas;
+    });
   }
-
 }
