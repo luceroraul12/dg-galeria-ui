26f7a6796f8e7e27c8599f5a1b079624c5572a27
Lucero Raul <luceroraul12@gmail.com>
2022-09-09 22:31:03 -0300

    delete routermoudle

--- src/app/paginas/cliente/cliente.module.ts
+++ src/app/paginas/cliente/cliente.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { VistaClienteComponent } from './vista-cliente/vista-cliente.component';
 import { GaleriaComponent } from './vista-cliente/components/galeria/galeria.component';
 import { VistaDetalladaComponent } from './vista-cliente/components/vista-detallada/vista-detallada.component';
-import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from '../../app-routing.module';
 
 @NgModule({
