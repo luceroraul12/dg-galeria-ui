de586bd62e01af1fc68f3ed4961511740eb094ca
Lucero Raul <luceroraul12@gmail.com>
2022-09-09 23:17:33 -0300

    adaptando de nuevp

--- src/app/paginas/cliente/vista-cliente/components/vista-detallada/vista-detallada.component.ts
+++ src/app/paginas/cliente/vista-cliente/components/vista-detallada/vista-detallada.component.ts
@@ -1,5 +1,10 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Marca } from 'src/app/interfaces/marca.interface';
+import { MarcaService } from '../../../../../servicios/marca.service';
+import { SaborService } from '../../../../../servicios/sabor.service';
+import { Sabor } from 'src/app/interfaces/sabor.interface';
+import { SaborFormateado } from '../../../../../interfaces/saborformateado.interface';
 
 @Component({
   selector: 'app-vista-detallada',
@@ -10,8 +15,15 @@ export class VistaDetalladaComponent implements OnInit {
   @Output() fueClickeadoCambio = new EventEmitter<boolean>();
 
   private idMarca: string = '';
+  public marcaElegida!: Marca;
+  public saboresDisponibles!: Sabor[];
+  public saboresFormateados!: SaborFormateado[];
 
-  constructor(private activateRoute: ActivatedRoute) {}
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private marcaService: MarcaService,
+    private saborService: SaborService
+  ) {}
 
   ngOnInit(): void {}
 }
