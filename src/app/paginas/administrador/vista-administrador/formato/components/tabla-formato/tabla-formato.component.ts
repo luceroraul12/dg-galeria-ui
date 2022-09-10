b05e2c499a9e816b6e8abc3c02ace0c99c5c163d
Lucero Raul <luceroraul12@gmail.com>
2022-09-10 16:47:02 -0300

    add conexion con api

--- src/app/paginas/administrador/vista-administrador/formato/components/tabla-formato/tabla-formato.component.ts
+++ src/app/paginas/administrador/vista-administrador/formato/components/tabla-formato/tabla-formato.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Formato } from '../../../../../../interfaces/formato.interface';
+import { FormatoService } from '../../../../../../servicios/formato.service';
 
 @Component({
   selector: 'app-tabla-formato',
   templateUrl: './tabla-formato.component.html',
-  styleUrls: ['./tabla-formato.component.css']
+  styleUrls: ['./tabla-formato.component.css'],
 })
 export class TablaFormatoComponent implements OnInit {
+  public formatosCreados: Formato[] = [];
 
-  constructor() { }
+  constructor(private formatoService: FormatoService) {}
 
   ngOnInit(): void {
+    this.formatoService
+      .getAllFormatos()
+      .subscribe(
+        (respuesta) => (this.formatosCreados = respuesta.formatosTrabajados)
+      );
   }
-
 }
