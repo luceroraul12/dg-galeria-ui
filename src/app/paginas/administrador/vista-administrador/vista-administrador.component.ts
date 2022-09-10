9d2ea8682ab8ea279d789729259d68900c763f4d
Lucero Raul <luceroraul12@gmail.com>
2022-09-10 12:41:40 -0300

    fix url imagenes

--- src/app/paginas/administrador/vista-administrador/vista-administrador.component.ts
+++ src/app/paginas/administrador/vista-administrador/vista-administrador.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-vista-administrador',
   templateUrl: './vista-administrador.component.html',
-  styleUrls: ['./vista-administrador.component.css']
+  styleUrls: ['./vista-administrador.component.css'],
 })
 export class VistaAdministradorComponent implements OnInit {
+  public urlFormato: string = environment.urlImgFormato;
+  public urlMarca: string = environment.urlImgMarca;
+  public urlSabor: string = environment.urlImgSabor;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
