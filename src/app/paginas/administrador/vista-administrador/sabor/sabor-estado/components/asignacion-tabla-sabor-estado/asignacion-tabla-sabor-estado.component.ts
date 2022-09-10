4afc3e48c604eb938e5459f376c7e9863d621ed7
Lucero Raul <luceroraul12@gmail.com>
2022-09-10 18:28:26 -0300

    add conexion con api sabores formateados

--- src/app/paginas/administrador/vista-administrador/sabor/sabor-estado/components/asignacion-tabla-sabor-estado/asignacion-tabla-sabor-estado.component.ts
+++ src/app/paginas/administrador/vista-administrador/sabor/sabor-estado/components/asignacion-tabla-sabor-estado/asignacion-tabla-sabor-estado.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { SaborFormateado } from '../../../../../../../interfaces/saborformateado.interface';
+import { SaborFormateadoService } from '../../../../../../../servicios/sabor-formateado.service';
 
 @Component({
   selector: 'app-asignacion-tabla-sabor-estado',
   templateUrl: './asignacion-tabla-sabor-estado.component.html',
-  styleUrls: ['./asignacion-tabla-sabor-estado.component.css']
+  styleUrls: ['./asignacion-tabla-sabor-estado.component.css'],
 })
 export class AsignacionTablaSaborEstadoComponent implements OnInit {
+  public saboresFormateados: SaborFormateado[] = [];
 
-  constructor() { }
+  constructor(public saborFormateadoService: SaborFormateadoService) {}
 
   ngOnInit(): void {
+    this.saborFormateadoService
+      .getAllSaborFormateados()
+      .subscribe(
+        (respuesta) =>
+          (this.saboresFormateados = respuesta.saboresFormateadosTrabajados)
+      );
   }
-
 }
