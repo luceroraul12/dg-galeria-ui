7ff297c5b30365f902757b950759a3f22a326e79
Lucero Raul <luceroraul12@gmail.com>
2022-09-10 12:18:59 -0300

    add observable

--- src/app/servicios/cliente.service.ts
+++ src/app/servicios/cliente.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ClienteResponse } from '../interfaces/cliente.interface';
+import { Marca } from '../interfaces/marca.interface';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClienteService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  resumen(idMarca: string): Observable<ClienteResponse> {
+    let url = `${environment.apiCliente}?id-marca=${idMarca}`;
+    return this.http.get<ClienteResponse>(url);
+  }
 }
