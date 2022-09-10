e293e976aff73a6fbbed5ea523863d6618acd919
Lucero Raul <luceroraul12@gmail.com>
2022-09-10 11:34:08 -0300

    add metodos

--- src/app/servicios/sabor-asociado.service.ts
+++ src/app/servicios/sabor-asociado.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import {
+  SaborAsociado,
+  SaborAsociadoResponse,
+} from '../interfaces/saborasociado.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SaborAsociadoService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllSaborAsociados(): Observable<SaborAsociadoResponse> {
+    return this.http.get<SaborAsociadoResponse>(environment.apiSaborAsociado);
+  }
+
+  getAllByIdMarca(idMarca: string): Observable<SaborAsociadoResponse> {
+    let params: HttpParams = new HttpParams().set('id', idMarca);
+    return this.http.get<SaborAsociadoResponse>(environment.apiSaborAsociado, {
+      params,
+    });
+  }
+
+  crearSaborAsociado(
+    saborAsociado: SaborAsociado
+  ): Observable<SaborAsociadoResponse> {
+    return this.http.post<SaborAsociadoResponse>(
+      environment.apiSaborAsociado,
+      saborAsociado
+    );
+  }
+
+  actualizarSaborAsociado(
+    body: SaborAsociado
+  ): Observable<SaborAsociadoResponse> {
+    return this.http.put<SaborAsociadoResponse>(
+      environment.apiSaborAsociado,
+      body
+    );
+  }
+
+  eliminarSaborAsociado(id: string): Observable<SaborAsociadoResponse> {
+    let params: HttpParams = new HttpParams().set('id', id);
+    return this.http.delete<SaborAsociadoResponse>(
+      environment.apiSaborAsociado,
+      {
+        params,
+      }
+    );
+  }
 }
