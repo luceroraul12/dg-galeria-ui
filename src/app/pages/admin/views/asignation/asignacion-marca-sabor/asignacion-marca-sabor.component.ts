30bd2176a129a8ac2b1eae9cf75d22f33858ea70
Lucero Raul <luceroraul12@gmail.com>
2022-09-15 02:17:53 -0300

    add template and subscription

--- src/app/pages/admin/views/asignation/asignacion-marca-sabor/asignacion-marca-sabor.component.ts
+++ src/app/pages/admin/views/asignation/asignacion-marca-sabor/asignacion-marca-sabor.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { BrandedTaste } from 'src/app/interfaces/branded-taste.interface';
+import { BrandedTasteService } from 'src/app/services/crud/branded-taste.service';
 
 @Component({
   selector: 'app-asignacion-marca-sabor',
   templateUrl: './asignacion-marca-sabor.component.html',
-  styleUrls: ['./asignacion-marca-sabor.component.css']
+  styleUrls: ['./asignacion-marca-sabor.component.css'],
 })
 export class AsignacionMarcaSaborComponent implements OnInit {
+  public brandedTasteData: BrandedTaste[] = [];
 
-  constructor() { }
+  constructor(private brandedTasteService: BrandedTasteService) {}
 
   ngOnInit(): void {
+    this.brandedTasteService
+      .read()
+      .subscribe(
+        (response) => (this.brandedTasteData = response.stockDataResult)
+      );
   }
-
 }
