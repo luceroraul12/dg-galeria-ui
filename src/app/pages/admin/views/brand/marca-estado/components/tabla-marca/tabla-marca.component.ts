6477fe791fa2cb833bcbfee2dca42d06f22e7b85
Lucero Raul <luceroraul12@gmail.com>
2022-09-15 19:22:53 -0300

    extend callInputsTable and tableService

--- src/app/pages/admin/views/brand/marca-estado/components/tabla-marca/tabla-marca.component.ts
+++ src/app/pages/admin/views/brand/marca-estado/components/tabla-marca/tabla-marca.component.ts
@@ -1,21 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Brand } from 'src/app/interfaces/brand.interface';
+import { CallInputsTable } from 'src/app/services/call-inputs-table.service';
 import { BrandService } from 'src/app/services/crud/brand.service';
+import { TableService } from 'src/app/services/table.service';
 
 @Component({
   selector: 'app-tabla-marca',
   templateUrl: './tabla-marca.component.html',
   styleUrls: ['./tabla-marca.component.css'],
 })
-export class TablaMarcaComponent implements OnInit {
+export class TablaMarcaComponent
+  extends CallInputsTable<Brand>
+  implements OnInit
+{
   public marcasCreadas: Brand[] = [];
   @Input() tipoDeTabla: string = 'determinado';
 
-  constructor(private brandService: BrandService) {}
+  constructor(private brandService: BrandService, tableService: TableService) {
+    super(tableService);
+  }
 
   ngOnInit(): void {
-    this.brandService
-      .read()
-      .subscribe((response) => (this.marcasCreadas = response.stockDataResult));
+    this.brandService.read().subscribe((response) => {
+      this.marcasCreadas = response.stockDataResult;
+      this.tableService.stockDataTable = response.stockDataResult;
+    });
   }
 }
