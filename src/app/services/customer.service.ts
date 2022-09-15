bf9c2320de5ee1225eb28808a6ba435482a49696
Lucero Raul <luceroraul12@gmail.com>
2022-09-15 01:28:09 -0300

    add customer service

---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Brand } from '../interfaces/brand.interface';
+import { CustomerResponse } from '../interfaces/customer.response.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomerService {
+  private url: string = environment.apiCliente;
+
+  constructor(private http: HttpClient) {}
+
+  resultByIdBrand(idBrand: number): Observable<CustomerResponse> {
+    let params = new HttpParams().set('id-brand', idBrand);
+    return this.http.get<CustomerResponse>(this.url, { params });
+  }
+}
