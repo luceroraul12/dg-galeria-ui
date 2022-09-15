e9f4ca10c0761c9bbe90b65116f9b96f0821faaa
Lucero Raul <luceroraul12@gmail.com>
2022-09-15 01:38:21 -0300

    add subscribe and fix template attrs

--- src/app/pages/customer/views/gallery/detailed-gallery/vista-detallada.component.ts
+++ src/app/pages/customer/views/gallery/detailed-gallery/vista-detallada.component.ts
@@ -1,5 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
+import { Brand } from 'src/app/interfaces/brand.interface';
+import {
+  CustomerResponse,
+  TasteResult,
+} from 'src/app/interfaces/customer.response.interface';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-vista-detallada',
@@ -10,12 +17,22 @@ export class VistaDetalladaComponent implements OnInit {
   @Output() fueClickeadoCambio = new EventEmitter<boolean>();
 
   private idMarca: string = '';
-  public marcaElegida!: any;
-  public saboresCreados!: any[];
+  public marcaElegida!: Brand;
+  public saboresCreados!: TasteResult[];
 
-  constructor(private activateRoute: ActivatedRoute) {}
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private customerService: CustomerService
+  ) {}
 
   ngOnInit(): void {
-    this.activateRoute.params.subscribe(console.log);
+    this.activateRoute.params.subscribe(({ idMarca }) =>
+      this.customerService
+        .resultByIdBrand(idMarca)
+        .subscribe((response: CustomerResponse) => {
+          this.saboresCreados = response.customerResult.tasteResults;
+          this.marcaElegida = response.customerResult.brandSelected;
+        })
+    );
   }
 }
