5196cba0a19a56b6674d9cfbd3aacaf35fcc973f
Lucero Raul <luceroraul12@gmail.com>
2022-09-15 18:44:29 -0300

    rename and add link functions to table buttons

--- src/app/pages/admin/views/drink-container/components/tabla-formato/tabla-formato.component.ts
+++ src/app/pages/admin/views/drink-container/components/tabla-formato/tabla-formato.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DrinkContainer } from 'src/app/interfaces/drink-container.interface';
 import { DrinkContainerService } from 'src/app/services/crud/drink-container.service';
+import { TableService } from 'src/app/services/table.service';
 
 @Component({
   selector: 'app-tabla-formato',
@@ -8,15 +9,35 @@ import { DrinkContainerService } from 'src/app/services/crud/drink-container.ser
   styleUrls: ['./tabla-formato.component.css'],
 })
 export class TablaFormatoComponent implements OnInit {
-  public formatosCreados: DrinkContainer[] = [];
+  public drinkContainerCreated: DrinkContainer[] = [];
 
-  constructor(private drinkContainerService: DrinkContainerService) {}
+  constructor(
+    private drinkContainerService: DrinkContainerService,
+    private tableService: TableService
+  ) {}
 
   ngOnInit(): void {
-    this.drinkContainerService
-      .read()
-      .subscribe(
-        (response) => (this.formatosCreados = response.stockDataResult)
-      );
+    this.drinkContainerService.read().subscribe((response) => {
+      this.drinkContainerCreated = response.stockDataResult;
+      this.tableService.stockDataTable = response.stockDataResult;
+    });
+    this.tableService.stockDataTable = this.drinkContainerCreated;
+  }
+
+  delete(drinkContainer: DrinkContainer): void {
+    console.log('delete tabla formato');
+
+    this.tableService.deleteRowData(drinkContainer);
+  }
+
+  update(drinkContainer: DrinkContainer): void {
+    console.log('update tabla formato');
+
+    this.tableService.updateRowData(drinkContainer);
+  }
+
+  changeStockState(drinkContainer: DrinkContainer): void {
+    console.log('changeState tabla formato');
+    this.tableService.changeStockState(drinkContainer);
   }
 }
