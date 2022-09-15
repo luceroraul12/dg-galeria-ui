1f859fc76cfb6319162c3fe8decb835180b73af1
Lucero Raul <luceroraul12@gmail.com>
2022-09-15 19:12:51 -0300

    behavior -> call button methods

--- src/app/pages/admin/views/drink-container/components/tabla-formato/tabla-formato.component.ts
+++ src/app/pages/admin/views/drink-container/components/tabla-formato/tabla-formato.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DrinkContainer } from 'src/app/interfaces/drink-container.interface';
+import { CallInputsTable } from 'src/app/services/call-inputs-table.service';
 import { DrinkContainerService } from 'src/app/services/crud/drink-container.service';
 import { TableService } from 'src/app/services/table.service';
 
@@ -8,13 +9,18 @@ import { TableService } from 'src/app/services/table.service';
   templateUrl: './tabla-formato.component.html',
   styleUrls: ['./tabla-formato.component.css'],
 })
-export class TablaFormatoComponent implements OnInit {
+export class TablaFormatoComponent
+  extends CallInputsTable<DrinkContainer>
+  implements OnInit
+{
   public drinkContainerCreated: DrinkContainer[] = [];
 
   constructor(
     private drinkContainerService: DrinkContainerService,
-    private tableService: TableService
-  ) {}
+    tableService: TableService
+  ) {
+    super(tableService);
+  }
 
   ngOnInit(): void {
     this.drinkContainerService.read().subscribe((response) => {
@@ -22,22 +28,5 @@ export class TablaFormatoComponent implements OnInit {
       this.tableService.stockDataTable = response.stockDataResult;
     });
     this.tableService.stockDataTable = this.drinkContainerCreated;
-  }
-
-  delete(drinkContainer: DrinkContainer): void {
-    console.log('delete tabla formato');
-
-    this.tableService.deleteRowData(drinkContainer);
-  }
-
-  update(drinkContainer: DrinkContainer): void {
-    console.log('update tabla formato');
-
-    this.tableService.updateRowData(drinkContainer);
-  }
-
-  changeStockState(drinkContainer: DrinkContainer): void {
-    console.log('changeState tabla formato');
-    this.tableService.changeStockState(drinkContainer);
   }
 }
