43399d93807cbdb3e276b1a057e035e7c4d45c83
Lucero Raul <luceroraul12@gmail.com>
2022-09-15 01:46:53 -0300

    add extends

--- src/app/services/crud/drink-container.service.ts
+++ src/app/services/crud/drink-container.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DrinkContainer } from 'src/app/interfaces/drink-container.interface';
+import { environment } from 'src/environments/environment';
+import { CrudService } from './crud-abstract.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DrinkContainerService {
+export class DrinkContainerService extends CrudService<DrinkContainer> {
+  urlApi: string = environment.apiFormato;
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super(http);
+  }
 }
