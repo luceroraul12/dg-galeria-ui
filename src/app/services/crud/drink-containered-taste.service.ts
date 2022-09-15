fa6058af08bb91721593e3d9c03a2416b8324dd9
Lucero Raul <luceroraul12@gmail.com>
2022-09-15 02:19:08 -0300

    add extends

--- src/app/services/crud/drink-containered-taste.service.ts
+++ src/app/services/crud/drink-containered-taste.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DrinkContaineredTaste } from 'src/app/interfaces/drink-containered-taste.interface';
+import { environment } from 'src/environments/environment';
+import { CrudService } from './crud-abstract.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DrinkContaineredTasteService {
+export class DrinkContaineredTasteService extends CrudService<DrinkContaineredTaste> {
+  urlApi: string = environment.apiSaborFormateado;
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super(http);
+  }
 }
