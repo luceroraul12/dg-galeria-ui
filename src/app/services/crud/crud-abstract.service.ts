aec899696a43562cc7e200feee86171fcd1deba7
Lucero Raul <luceroraul12@gmail.com>
2022-09-15 00:47:26 -0300

    add abstract simple crud

---
+++ src/app/services/crud/crud-abstract.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { StockDataResponse } from 'src/app/interfaces/response.interface';
+import { StockData } from 'src/app/interfaces/stock-data.interface';
+
+export abstract class CrudService<Entity extends StockData> {
+  private urlApi!: string;
+
+  constructor(private http: HttpClient) {}
+
+  create(element: Entity): Observable<StockDataResponse<Entity>> {
+    return this.http.post<StockDataResponse<Entity>>(this.urlApi, element);
+  }
+
+  read(): Observable<StockDataResponse<Entity>> {
+    return this.http.get<StockDataResponse<Entity>>(this.urlApi);
+  }
+
+  update(element: Entity): Observable<StockDataResponse<Entity>> {
+    return this.http.put<StockDataResponse<Entity>>(this.urlApi, element);
+  }
+
+  delete(element: Entity): Observable<StockDataResponse<Entity>> {
+    return this.http.delete<StockDataResponse<Entity>>(this.urlApi);
+  }
+}
