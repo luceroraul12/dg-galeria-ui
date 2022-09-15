8e93a5c55f48bbf799c67fa31bd2cacbe97c0436
Lucero Raul <luceroraul12@gmail.com>
2022-09-15 16:06:56 -0300

    img separated for urls api

--- src/app/pages/admin/views/vista-administrador.component.ts
+++ src/app/pages/admin/views/vista-administrador.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-vista-administrador',
@@ -7,12 +6,6 @@ import { environment } from '../../../../environments/environment';
   styleUrls: ['./vista-administrador.component.css'],
 })
 export class VistaAdministradorComponent implements OnInit {
-  public urlFormato: string = environment.urlImgFormato;
-  public urlMarca: string = environment.urlImgMarca;
-  public urlSabor: string = environment.urlImgSabor;
-  public urlAsingacionMarcaSabor: string =
-    environment.urlImgAsignacionMarcaSabor;
-
   constructor() {}
 
   ngOnInit(): void {}
