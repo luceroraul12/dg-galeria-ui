5ffe96605bd01ff0b25a2477187c19b7b7ff6a11
Lucero Raul <luceroraul12@gmail.com>
2022-09-16 09:30:30 -0300

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AsignacionMarcaSaborComponent } from './pages/admin/views/asignation/asignacion-marca-sabor/asignacion-marca-sabor.component';
 import { AsignacionSaborFormatoComponent } from './pages/admin/views/asignation/asignacion-sabor-formato/asignacion-sabor-formato.component';
-import { MarcaEstadoComponent } from './pages/admin/views/brand/marca-estado/marca-estado.component';
+import { MarcaEstadoComponent } from './pages/admin/views/brand/brand-state/brand-state.component';
 import { MarcaComponent } from './pages/admin/views/brand/marca.component';
 import { FormatoComponent } from './pages/admin/views/drink-container/formato.component';
 import { SaborEstadoComponent } from './pages/admin/views/taste/sabor-estado/sabor-estado.component';
