0e4341821fffbcd51321ef3528ba83a6299f1d2e
Lucero Raul <luceroraul12@gmail.com>
2022-09-16 16:52:55 -0300

    add sort by brand name

--- src/app/pages/admin/views/mixed/branded-taste/branded-taste.component.ts
+++ src/app/pages/admin/views/mixed/branded-taste/branded-taste.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { BrandedTaste } from 'src/app/interfaces/branded-taste.interface';
+import { CallInputsTable } from 'src/app/services/call-inputs-table.service';
 import { BrandedTasteService } from 'src/app/services/crud/branded-taste.service';
+import { TableService } from 'src/app/services/table.service';
 
 @Component({
   selector: 'app-branded-taste',
   templateUrl: './branded-taste.component.html',
   styleUrls: ['./branded-taste.component.css'],
 })
-export class AsignacionMarcaSaborComponent implements OnInit {
+export class AsignacionMarcaSaborComponent
+  extends CallInputsTable<BrandedTaste>
+  implements OnInit
+{
   public brandedTasteData: BrandedTaste[] = [];
 
-  constructor(private brandedTasteService: BrandedTasteService) {}
+  constructor(
+    private brandedTasteService: BrandedTasteService,
+    tableService: TableService
+  ) {
+    super(tableService, brandedTasteService);
+  }
 
   ngOnInit(): void {
-    this.brandedTasteService
-      .read()
-      .subscribe(
-        (response) => (this.brandedTasteData = response.stockDataResult)
-      );
+    this.brandedTasteService.read().subscribe(({ stockDataResult }) => {
+      stockDataResult.sort();
+      this.brandedTasteData = stockDataResult;
+    });
   }
 }
