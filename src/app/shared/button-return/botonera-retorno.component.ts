2a726c305fca9c3403ad4d20a3b58dfa34b08d21
Lucero Raul <luceroraul12@gmail.com>
2022-09-16 10:29:52 -0300

    add input message

--- src/app/shared/button-return/botonera-retorno.component.ts
+++ src/app/shared/button-return/botonera-retorno.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BotoneraRetornoComponent implements OnInit {
   @Input() ruta: string = '';
+  @Input() message: string = 'marcas';
 
   constructor() {}
 
