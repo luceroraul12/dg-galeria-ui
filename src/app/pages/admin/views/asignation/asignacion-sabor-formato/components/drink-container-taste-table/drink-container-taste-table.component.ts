2f056651fc888677f8e9b6fbde7573e4383cb569
Lucero Raul <luceroraul12@gmail.com>
2022-09-16 12:12:55 -0300

    add services and link events

--- src/app/pages/admin/views/asignation/asignacion-sabor-formato/components/drink-container-taste-table/drink-container-taste-table.component.ts
+++ src/app/pages/admin/views/asignation/asignacion-sabor-formato/components/drink-container-taste-table/drink-container-taste-table.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TableType } from 'src/app/constants/table-type';
 import { DrinkContaineredTaste } from 'src/app/interfaces/drink-containered-taste.interface';
+import { CallInputsTable } from 'src/app/services/call-inputs-table.service';
 import { DrinkContaineredTasteService } from 'src/app/services/crud/drink-containered-taste.service';
+import { TableService } from 'src/app/services/table.service';
 
 @Component({
   selector: 'app-drink-container-taste-table',
   templateUrl: './drink-container-taste-table.component.html',
   styleUrls: ['./drink-container-taste-table.component.css'],
 })
-export class DrinkContainerTasteTableComponent implements OnInit {
+export class DrinkContainerTasteTableComponent
+  extends CallInputsTable<DrinkContaineredTaste>
+  implements OnInit
+{
   public drinkContainerTasteData: DrinkContaineredTaste[] = [];
 
+  @Input() tableType: TableType = TableType.STOCK_STATE;
+
   constructor(
-    private drinkContainerTasteService: DrinkContaineredTasteService
-  ) {}
+    private drinkContainerTasteService: DrinkContaineredTasteService,
+    tableService: TableService
+  ) {
+    super(tableService);
+  }
 
   ngOnInit(): void {
-    this.drinkContainerTasteService
-      .read()
-      .subscribe(
-        (response) => (this.drinkContainerTasteData = response.stockDataResult)
-      );
+    this.drinkContainerTasteService.read().subscribe((response) => {
+      this.drinkContainerTasteData = response.stockDataResult;
+      this.tableService.stockDataTable = response.stockDataResult;
+    });
   }
 }
