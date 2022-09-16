5f4c6fc7de87de665b9177573db4da2bd93d79c2
Lucero Raul <luceroraul12@gmail.com>
2022-09-16 14:16:58 -0300

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiCliente: 'https://dg-galeria-api.herokuapp.com/customer',
   apiFormato: 'https://dg-galeria-api.herokuapp.com/drink-containers',
-  apiMarca: 'httpss://dg-galeria-api.herokuapp.com/brands',
+  apiMarca: 'https://dg-galeria-api.herokuapp.com/brands',
   apiSabor: 'https://dg-galeria-api.herokuapp.com/tastees',
   apiSaborAsociado: 'https://dg-galeria-api.herokuapp.com/branded-taste',
   apiSaborFormateado:
