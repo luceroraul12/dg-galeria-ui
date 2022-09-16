5da652b9be3dc820d7ba8e5695f96a6e35448995
Lucero Raul <luceroraul12@gmail.com>
2022-09-16 14:13:38 -0300

    fix routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiCliente: 'http://dg-galeria-api.herokuapp.com/customer',
-  apiFormato: 'http://dg-galeria-api.herokuapp.com/drink-containers',
-  apiMarca: 'https://dg-galeria-api.herokuapp.com/brands',
-  apiSabor: 'http://dg-galeria-api.herokuapp.com/tastees',
-  apiSaborAsociado: 'http://dg-galeria-api.herokuapp.com/branded-taste',
+  apiCliente: 'https://dg-galeria-api.herokuapp.com/customer',
+  apiFormato: 'https://dg-galeria-api.herokuapp.com/drink-containers',
+  apiMarca: 'httpss://dg-galeria-api.herokuapp.com/brands',
+  apiSabor: 'https://dg-galeria-api.herokuapp.com/tastees',
+  apiSaborAsociado: 'https://dg-galeria-api.herokuapp.com/branded-taste',
   apiSaborFormateado:
-    'http://dg-galeria-api.herokuapp.com/drink-containered-taste',
+    'https://dg-galeria-api.herokuapp.com/drink-containered-taste',
 };
