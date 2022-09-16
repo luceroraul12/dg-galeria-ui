ed265c636fff9dac5d8b7c674241e59a46a61c41
Lucero Raul <luceroraul12@gmail.com>
2022-09-16 11:12:49 -0300

    add services and fix template

--- src/app/pages/admin/views/taste/components/taste-table/tabla-sabor.component.ts
+++ src/app/pages/admin/views/taste/components/taste-table/tabla-sabor.component.ts
@@ -1,22 +1,33 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TableType } from 'src/app/constants/table-type';
 import { Taste } from 'src/app/interfaces/taste.interface';
+import { CallInputsTable } from 'src/app/services/call-inputs-table.service';
 import { TasteService } from 'src/app/services/crud/taste.service';
+import { TableService } from 'src/app/services/table.service';
 
 @Component({
   selector: 'app-tabla-sabor',
   templateUrl: './tabla-sabor.component.html',
   styleUrls: ['./tabla-sabor.component.css'],
 })
-export class TablaSaborComponent implements OnInit {
-  @Input() tipoDeTabla: string = 'determinado';
+export class TablaSaborComponent
+  extends CallInputsTable<Taste>
+  implements OnInit
+{
+  @Input() tableType: TableType = TableType.ASIGNATION;
   public saboresCreados: Taste[] = [];
-  constructor(private tasteService: TasteService) {}
+  constructor(private tasteService: TasteService, tableService: TableService) {
+    super(tableService);
+  }
 
   ngOnInit(): void {
-    this.tasteService
-      .read()
-      .subscribe(
-        (response) => (this.saboresCreados = response.stockDataResult)
-      );
+    this.tasteService.read().subscribe((response) => {
+      this.saboresCreados = response.stockDataResult;
+      this.tableService.stockDataTable = response.stockDataResult;
+    });
+  }
+
+  isStockState() {
+    return this.tableType == TableType.STOCK_STATE;
   }
 }
