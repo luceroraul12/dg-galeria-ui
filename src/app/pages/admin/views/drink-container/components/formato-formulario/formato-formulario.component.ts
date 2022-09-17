a7a5dd3fca64403d02bd4f5c1dfe274ecadb7b95
Lucero Raul <luceroraul12@gmail.com>
2022-09-17 11:06:54 -0300

    add apiservice. create

--- src/app/pages/admin/views/drink-container/components/formato-formulario/formato-formulario.component.ts
+++ src/app/pages/admin/views/drink-container/components/formato-formulario/formato-formulario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { DrinkContainer } from 'src/app/interfaces/drink-container.interface';
+import { DrinkContainerService } from 'src/app/services/crud/drink-container.service';
 
 @Component({
   selector: 'app-formato-formulario',
@@ -14,12 +15,17 @@ export class FormatoFormularioComponent implements OnInit {
     isStocked: true,
   };
 
-  constructor() {}
+  constructor(private drinkContainerService: DrinkContainerService) {}
 
   ngOnInit(): void {}
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    console.log(this.drinkContianer.containerName);
+    this.drinkContainerService
+      .create(this.drinkContianer)
+      .subscribe(({ stockDataResult }) => {
+        console.log(stockDataResult[0]);
+      });
   }
 
   onCancel(form: NgForm) {
