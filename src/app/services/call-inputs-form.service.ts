b8e03e1c83e7286356dc5d5dc036d39612de1f7b
Lucero Raul <luceroraul12@gmail.com>
2022-09-17 12:04:10 -0300

    add first methods

--- src/app/services/call-inputs-form.service.ts
+++ src/app/services/call-inputs-form.service.ts
@@ -1,1 +1,23 @@
-export abstract class CallInputsForm {}
+import { StockData } from '../interfaces/stock-data.interface';
+import { CrudService } from './crud/crud-abstract.service';
+import { TableService } from './table.service';
+
+export abstract class CallInputsForm<Entity extends StockData> {
+  constructor(
+    private tableService: TableService,
+    private crudService: CrudService<Entity>
+  ) {}
+  create(formData: Entity) {
+    this.crudService.create(formData).subscribe(({ stockDataResult }) => {
+      console.log(stockDataResult[0]);
+      this.tableService.addRowData(stockDataResult[0]);
+    });
+  }
+
+  update(formData: Entity) {
+    this.crudService.update(formData).subscribe(({ stockDataResult }) => {
+      console.log(stockDataResult[0]);
+      this.tableService.updateRowData(stockDataResult[0]);
+    });
+  }
+}
