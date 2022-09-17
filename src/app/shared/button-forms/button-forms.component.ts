0e20250c87f3f334f89d92cb0417bde310f30911
Lucero Raul <luceroraul12@gmail.com>
2022-09-17 09:55:28 -0300

    add events

--- src/app/shared/button-forms/button-forms.component.ts
+++ src/app/shared/button-forms/button-forms.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button-forms',
   templateUrl: './button-forms.component.html',
-  styleUrls: ['./button-forms.component.css']
+  styleUrls: ['./button-forms.component.css'],
 })
 export class ButtonFormsComponent implements OnInit {
+  @Output() submit: EventEmitter<boolean> = new EventEmitter();
+  @Output() cancel: EventEmitter<boolean> = new EventEmitter();
+  @Output() addMany: EventEmitter<boolean> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.submit.emit(true);
   }
 
+  onCancel() {
+    this.cancel.emit(true);
+  }
+
+  onAddMany() {
+    this.addMany.emit(true);
+  }
 }
