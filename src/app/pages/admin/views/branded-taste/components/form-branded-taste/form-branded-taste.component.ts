f18b75af29dd339ec12eacafbab05c33efef25e2
Lucero Raul <luceroraul12@gmail.com>
2022-09-19 11:25:43 -0300

    add form, i must connect it

--- src/app/pages/admin/views/branded-taste/components/form-branded-taste/form-branded-taste.component.ts
+++ src/app/pages/admin/views/branded-taste/components/form-branded-taste/form-branded-taste.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs';
 import { FormAbstractComponent } from 'src/app/abstract/components/form.abstract.component';
 import { TableService } from 'src/app/services/table.service';
+import { Brand } from '../../../brand/interface/brand.interface';
+import { BrandService } from '../../../brand/service/brand.service';
+import { Taste } from '../../../taste/interface/taste.interface';
+import { TasteService } from '../../../taste/service/taste.service';
 import { BrandedTaste } from '../../interface/branded-taste.interface';
 import { BrandedTasteService } from '../../service/branded-taste.service';
 
@@ -13,18 +18,40 @@ export class FormBrandedTasteComponent
   extends FormAbstractComponent<BrandedTaste>
   implements OnInit
 {
+  public registeredBrands: Brand[] = [];
+  public registeredTastes: Taste[] = [];
+
   validate(): boolean {
     return false;
   }
   reset(): void {}
   constructor(
     tableService: TableService<BrandedTaste>,
-    brandedTasteService: BrandedTasteService
+    brandedTasteService: BrandedTasteService,
+    private brandService: BrandService,
+    private tasteService: TasteService
   ) {
     super(tableService, brandedTasteService);
   }
 
   ngOnInit(): void {
     this.initForm();
+    this.extraInit();
+  }
+
+  extraInit() {
+    this.brandService
+      .read()
+      .pipe(first())
+      .subscribe(
+        ({ stockDataResult }) => (this.registeredBrands = stockDataResult)
+      );
+
+    this.tasteService
+      .read()
+      .pipe(first())
+      .subscribe(
+        ({ stockDataResult }) => (this.registeredTastes = stockDataResult)
+      );
   }
 }
