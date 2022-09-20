96afc5224234e36a03a9be277614604d5142af9b
Lucero Raul <luceroraul12@gmail.com>
2022-09-20 20:44:06 -0300

    add form many branded taste service

---
+++ src/app/pages/admin/views/branded-taste/service/form-many-branded-taste.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { FormManyItemAbstractService } from 'src/app/abstract/service/form-many-item-abstract.service';
+import { BrandedTaste } from '../interface/branded-taste.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FormManyBrandedTasteService extends FormManyItemAbstractService<BrandedTaste> {
+  constructor() {
+    super();
+  }
+}
