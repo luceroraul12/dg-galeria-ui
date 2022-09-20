785026b13a887887f4f23472fb78ccd13c4fbfa5
Lucero Raul <luceroraul12@gmail.com>
2022-09-20 10:42:40 -0300

    brand name requiered

--- src/app/pages/admin/views/brand/interface/brand.interface.ts
+++ src/app/pages/admin/views/brand/interface/brand.interface.ts
@@ -2,7 +2,7 @@ import { StockData } from 'src/app/interfaces/stock-data.interface';
 
 export interface Brand extends StockData {
   id?: number;
-  brandName?: string;
+  brandName: string;
   url?: string;
   isStocked?: boolean;
 }
