41127f4d954ed980fc5d54842127cd2685353cf1
Lucero Raul <luceroraul12@gmail.com>
2022-09-19 22:15:38 -0300

    add form many item service

--- src/app/pages/admin/views/branded-taste/components/form-many-branded-taste/form-many-branded-taste.component.ts
+++ src/app/pages/admin/views/branded-taste/components/form-many-branded-taste/form-many-branded-taste.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs';
+import { Taste } from '../../../taste/interface/taste.interface';
+import { TasteService } from '../../../taste/service/taste.service';
+import { BrandedTaste } from '../../interface/branded-taste.interface';
 
 @Component({
   selector: 'app-form-many-branded-taste',
   templateUrl: './form-many-branded-taste.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class FormManyBrandedTasteComponent implements OnInit {
+  public registeredTastes: Taste[] = [];
+  public addedTastes: Taste[] = [];
 
-  constructor() { }
+  constructor(private tasteService: TasteService) {}
 
   ngOnInit(): void {
+    this.tasteService
+      .read()
+      .pipe(first())
+      .subscribe(({ stockDataResult }) => {
+        stockDataResult.sort((a, b) =>
+          a.tasteName!.localeCompare(b.tasteName!)
+        );
+        this.registeredTastes = stockDataResult;
+      });
   }
 
+  changeCheckbox(item: Taste) {
+    if (!this.addedTastes.includes(item)) {
+      this.addedTastes.push(item);
+    } else {
+      this.addedTastes = this.addedTastes.filter((x) => x != item);
+    }
+    console.log(this.addedTastes);
+  }
 }
