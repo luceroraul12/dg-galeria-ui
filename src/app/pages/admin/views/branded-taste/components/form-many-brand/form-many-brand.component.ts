fcb690bde05e18d9383f866a82453fbc1f382f4b
Lucero Raul <luceroraul12@gmail.com>
2022-09-20 19:26:22 -0300

    imp abstract component

--- src/app/pages/admin/views/branded-taste/components/form-many-brand/form-many-brand.component.ts
+++ src/app/pages/admin/views/branded-taste/components/form-many-brand/form-many-brand.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormManyItemAbstractComponent } from 'src/app/abstract/components/form.many.item.abstract.component';
+import { Brand } from '../../../brand/interface/brand.interface';
+import { BrandService } from '../../../brand/service/brand.service';
+import { FormManyBrandService } from '../../service/form-many-brand.service';
 
 @Component({
   selector: 'app-form-many-brand',
   templateUrl: './form-many-brand.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class FormManyBrandComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class FormManyBrandComponent extends FormManyItemAbstractComponent<Brand> {
+  wayToSort(a: Brand, b: Brand): number {
+    return a.brandName.localeCompare(b.brandName);
   }
 
+  constructor(
+    brandService: BrandService,
+    formManyBrandService: FormManyBrandService
+  ) {
+    super(brandService, formManyBrandService);
+  }
 }
