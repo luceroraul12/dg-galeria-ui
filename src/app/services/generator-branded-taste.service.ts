4b9e633b608335f8f2bf2f77646e7598d8e418a7
Lucero Raul <luceroraul12@gmail.com>
2022-09-19 23:57:41 -0300

    imp

--- src/app/services/generator-branded-taste.service.ts
+++ src/app/services/generator-branded-taste.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Brand } from '../pages/admin/views/brand/interface/brand.interface';
+import { BrandedTaste } from '../pages/admin/views/branded-taste/interface/branded-taste.interface';
+import { Taste } from '../pages/admin/views/taste/interface/taste.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GeneratorBrandedTasteService {
+  public selectedBrands!: Brand[];
+  public selectedTastes!: Taste[];
 
-  constructor() { }
+  private _createdBrandTastes!: BrandedTaste[];
+
+  public reset() {
+    this._createdBrandTastes = [];
+  }
+
+  generate(): BrandedTaste[] {
+    this.selectedBrands.forEach((brand) => {
+      this.selectedTastes.forEach((taste) => {
+        this._createdBrandTastes.push({
+          brand,
+          taste,
+        });
+      });
+    });
+    return this._createdBrandTastes;
+  }
 }
