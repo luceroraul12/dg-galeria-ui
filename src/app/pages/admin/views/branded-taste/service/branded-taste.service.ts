9c80c13468463c1912b0537ff2425eeac592a8e4
Lucero Raul <luceroraul12@gmail.com>
2022-09-20 02:22:11 -0300

    add create many

--- src/app/pages/admin/views/branded-taste/service/branded-taste.service.ts
+++ src/app/pages/admin/views/branded-taste/service/branded-taste.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StockDataResponse } from 'src/app/interfaces/response.interface';
 import { BrandedTaste } from 'src/app/pages/admin/views/branded-taste/interface/branded-taste.interface';
 import { environment } from 'src/environments/environment';
 import { CrudService } from '../../../../../abstract/service/crud-abstract.service';
@@ -12,5 +14,12 @@ export class BrandedTasteService extends CrudService<BrandedTaste> {
 
   constructor(http: HttpClient) {
     super(http);
+  }
+
+  createMany(
+    brandedTastes: BrandedTaste[]
+  ): Observable<StockDataResponse<BrandedTaste>> {
+    let url: string = `${this.urlApi}/monton`;
+    return this.http.post<StockDataResponse<BrandedTaste>>(url, brandedTastes);
   }
 }
