d3a1207ce4e488d89040b5ba8cbe48dc21d3733a
Lucero Raul <luceroraul12@gmail.com>
2022-09-20 22:39:02 -0300

    add create many

--- src/app/pages/admin/views/drink-containered-taste/service/drink-containered-taste.service.ts
+++ src/app/pages/admin/views/drink-containered-taste/service/drink-containered-taste.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StockDataResponse } from 'src/app/interfaces/response.interface';
 import { DrinkContaineredTaste } from 'src/app/pages/admin/views/drink-containered-taste/interface/drink-containered-taste.interface';
 import { environment } from 'src/environments/environment';
 import { CrudService } from '../../../../../abstract/service/crud-abstract.service';
@@ -12,5 +14,12 @@ export class DrinkContaineredTasteService extends CrudService<DrinkContaineredTa
 
   constructor(http: HttpClient) {
     super(http);
+  }
+
+  public createMany(
+    items: DrinkContaineredTaste[]
+  ): Observable<StockDataResponse<DrinkContaineredTaste>> {
+    let url: string = `${this.urlApi}/por-monton`;
+    return this.http.post<StockDataResponse<DrinkContaineredTaste>>(url, items);
   }
 }
