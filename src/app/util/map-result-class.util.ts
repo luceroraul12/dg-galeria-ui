387e607fdbbdaf7c79fb6da1bc1033be5041064e
Lucero Raul <luceroraul12@gmail.com>
2022-09-20 22:39:14 -0300

    add convert to drink containered taste

--- src/app/util/map-result-class.util.ts
+++ src/app/util/map-result-class.util.ts
@@ -1,5 +1,7 @@
 import { Brand } from '../pages/admin/views/brand/interface/brand.interface';
 import { BrandedTaste } from '../pages/admin/views/branded-taste/interface/branded-taste.interface';
+import { DrinkContainer } from '../pages/admin/views/drink-container/interface/drink-container.interface';
+import { DrinkContaineredTaste } from '../pages/admin/views/drink-containered-taste/interface/drink-containered-taste.interface';
 import { Taste } from '../pages/admin/views/taste/interface/taste.interface';
 import { ResultClass } from '../services/generator-result-class.service';
 
@@ -9,5 +11,16 @@ export class ResultClassUtil {
   ): BrandedTaste {
     let { primary, secundary } = result;
     return { brand: primary, taste: secundary, isStocked: true };
+  }
+
+  public static convertToDrinkContaineredTaste(
+    result: ResultClass<BrandedTaste, DrinkContainer>
+  ): DrinkContaineredTaste {
+    let { primary, secundary } = result;
+    return {
+      brandedTaste: primary,
+      drinkContainer: secundary,
+      isStocked: true,
+    };
   }
 }
