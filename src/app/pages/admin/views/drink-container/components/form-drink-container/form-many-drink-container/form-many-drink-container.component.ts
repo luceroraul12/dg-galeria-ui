d9795f13cdb4d7e418f7b3f4fd6b262235269528
Lucero Raul <luceroraul12@gmail.com>
2022-09-20 21:24:18 -0300

    add extends

--- src/app/pages/admin/views/drink-container/components/form-drink-container/form-many-drink-container/form-many-drink-container.component.ts
+++ src/app/pages/admin/views/drink-container/components/form-drink-container/form-many-drink-container/form-many-drink-container.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormManyItemAbstractComponent } from 'src/app/abstract/components/form.many.item.abstract.component';
+import { FormManyItemAbstractService } from 'src/app/abstract/service/form-many-item-abstract.service';
+import { DrinkContainer } from '../../../interface/drink-container.interface';
+import { DrinkContainerService } from '../../../service/drink-container.service';
+import { FormManyDrinkContainerService } from '../../../service/form-many-drink-container.service';
 
 @Component({
   selector: 'app-form-many-drink-container',
   templateUrl: './form-many-drink-container.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class FormManyDrinkContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class FormManyDrinkContainerComponent extends FormManyItemAbstractComponent<DrinkContainer> {
+  wayToSort(a: DrinkContainer, b: DrinkContainer): number {
+    return a.containerName!.localeCompare(b.containerName!);
   }
 
+  constructor(
+    drinkContainerService: DrinkContainerService,
+    formManyDrinkContainer: FormManyDrinkContainerService
+  ) {
+    super(drinkContainerService, formManyDrinkContainer);
+  }
 }
