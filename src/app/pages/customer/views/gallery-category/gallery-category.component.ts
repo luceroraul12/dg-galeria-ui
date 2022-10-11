1404c2dbcaceacf614827e3b6cd9f24fd791264b
Lucero Raul <luceroraul12@gmail.com>
2022-10-11 13:24:45 -0300

    add category brand template

--- src/app/pages/customer/views/gallery-category/gallery-category.component.ts
+++ src/app/pages/customer/views/gallery-category/gallery-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BrandCategory, CategoryName } from 'src/app/pages/admin/views/brand/interface/brand.interface';
 
 @Component({
   selector: 'app-gallery-category',
@@ -6,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery-category.component.css']
 })
 export class GalleryCategoryComponent implements OnInit {
+
+  public brandCategories: BrandCategory[] = [
+    {
+      categoryName: CategoryName.Cerveza,
+      id: 1
+    },
+    {
+      categoryName: CategoryName.Espumante,
+      id: 2
+    },
+    {
+      categoryName: CategoryName.Gaseosa,
+      id: 3
+    },
+    {
+      categoryName: CategoryName.Vino,
+      id: 4
+    },
+    {
+      categoryName: CategoryName.Otros,
+      id: 5
+    },
+  ];
 
   constructor() { }
 
