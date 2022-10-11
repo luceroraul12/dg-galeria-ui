04d783d173a3a19d9f887fac9528aacfdba6a571
Lucero Raul <luceroraul12@gmail.com>
2022-10-11 12:46:37 -0300

    add brandCategory interface

--- src/app/pages/admin/views/brand/interface/brand.interface.ts
+++ src/app/pages/admin/views/brand/interface/brand.interface.ts
@@ -6,4 +6,17 @@ export interface Brand extends StockData {
   url?: string;
   urlIconic?: string;
   isStocked?: boolean;
+  brandCategory?: BrandCategory;
+}
+export interface BrandCategory {
+  id:           number;
+  categoryName: CategoryName;
+}
+
+export enum CategoryName {
+  Cerveza = "CERVEZA",
+  Espumante = "ESPUMANTE",
+  Gaseosa = "GASEOSA",
+  Otros = "OTROS",
+  Vino = "VINO",
 }
