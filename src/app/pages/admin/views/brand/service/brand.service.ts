6a5279c57e4f1a6cb11bde0d45dfefc4f2b08f8b
Lucero Raul <luceroraul12@gmail.com>
2022-10-11 14:17:00 -0300

    add get categories

--- src/app/pages/admin/views/brand/service/brand.service.ts
+++ src/app/pages/admin/views/brand/service/brand.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Brand } from 'src/app/pages/admin/views/brand/interface/brand.interface';
+import { Observable } from 'rxjs';
+import { StockDataResponse } from 'src/app/interfaces/response.interface';
+import { Brand, BrandCategory } from 'src/app/pages/admin/views/brand/interface/brand.interface';
 import { environment } from 'src/environments/environment';
 import { CrudService } from '../../../../../abstract/service/crud-abstract.service';
 
@@ -11,5 +13,9 @@ export class BrandService extends CrudService<Brand> {
   urlApi: string = environment.apiMarca;
   constructor(http: HttpClient) {
     super(http);
+  }
+
+  categories(): Observable<StockDataResponse<BrandCategory>> {
+    return this.http.get<StockDataResponse<BrandCategory>>(`${this.urlApi}/categories`);
   }
 }
